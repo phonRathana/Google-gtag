569373d60d2b3308b64f05a9e1e78fabcded1c95
Phonrathana.github.io <127619322+phonRathana@users.noreply.github.com>
2023-09-14 03:17:08 +0700

    Update script.js

--- script.js
+++ script.js
@@ -3,9 +3,16 @@
   
   function onSignIn(googleUser) { 
   
-    var profile = googleUser.getBasicProfile(); 
-    console.log('ID: ' + profile.getId()); // Do not send to your backend! Use an ID token instead. 
-    console.log('Name: ' + profile.getName()); 
-    console.log('Image URL: ' + profile.getImageUrl()); 
-    console.log('Email: ' + profile.getEmail()); // This is null if the 'email' scope is not present. 
+      function  onsignIn () { 
+   var auth2 = gapi.auth2.getAuthInstance(); 
+  
+  <script type="text/javascript"/> 
+  
+         function onsignIn () 
+           { 
+            document.getElementById("signin").innerHTML.toggle("active"); 
+            document.getElementById("ul").innerHTML.toggle("link"); 
+        } 
+     } 
  }
+ 

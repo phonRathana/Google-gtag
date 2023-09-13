aefaaaab9b0d258d4575febcc3acd14a17efd720
Phonrathana.github.io <127619322+phonRathana@users.noreply.github.com>
2023-09-14 04:25:18 +0700

    Create Script.js

---
+++ imag/Script.js
@@ -0,0 +1,11 @@
+ function onSignIn(googleUser) {   
+       function  onsignIn () {  
+    var auth2 = gapi.auth2.getAuthInstance();   
+   <script type="text/javascript"/>    
+          function onsignIn ()  
+            {  
+             document.getElementById("signin").innerHTML.toggle("active");  
+             document.getElementById("ul").innerHTML.toggle("link");  
+         }  
+      }  
+  }
